ec09c374d891b5effb2aff8692f3a5d403a464b5
muratorgut <muratorgut@gmail.com>
2026-01-12 17:02:43 +0300

    Force webpack mode for build

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    turbopack: {
-        resolveAlias: {
-            canvas: false,
-        },
+    webpack: (config) => {
+        config.resolve.alias.canvas = false;
+        return config;
     },
 };
 
