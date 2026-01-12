cd34573b7770ec5bdf3cd5ae9954c9ffb6a34b02
muratorgut <muratorgut@gmail.com>
2026-01-12 16:52:49 +0300

    Fix Turbopack build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    turbopack: {},
     webpack: (config) => {
         config.resolve.alias.canvas = false;
         return config;
+    },
+    experimental: {
+        serverComponentsExternalPackages: ['@react-pdf/renderer'],
     },
 };
 
