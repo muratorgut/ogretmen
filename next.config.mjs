2f7a26afe15d05a9babfe55c8d3b284f245d3219
muratorgut <muratorgut@gmail.com>
2026-01-12 17:06:27 +0300

    Add vercel.json and optimize react-pdf config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                canvas: false,
+                fs: false,
+            };
+        }
         config.resolve.alias.canvas = false;
         return config;
+    },
+    experimental: {
+        serverComponentsExternalPackages: ['@react-pdf/renderer'],
     },
 };
 
